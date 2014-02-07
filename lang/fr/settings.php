e34b96f74342b6f4ba7c708fb7f7e1d8cce57cc0
jlgrall <jlgrall@users.noreply.github.com>
2014-02-07 21:24:19 +0100

    Add french translation.

---
+++ lang/fr/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * French language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Jiang Le <smartynaoki@gmail.com>
+ */
+ 
+
+$lang['frontmatter'] = "Activer front matter ? (Front mater est un bloc de texte au début des pages ayant un suffixe en .md. Commençant et finissant par ---. Le contenu du front mater ne sera pas inclus dans le markdown, mais sera accessible aux autres plugins. Utile par exemple avec le plugin \"tag\".)";
+$lang['markdowneditor'] = "Editeur markdown (pour les pages ayant un suffixe en .md)";
+$lang['markdowneditor_o_none'] = 'aucun';
+$lang['markdowneditor_o_meltdown'] = 'Meltdown';
